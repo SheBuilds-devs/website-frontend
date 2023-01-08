f8a6357ecd2a2b37a0481b8744f92dc9d1541140
gon araseon <72021422+ravikul505@users.noreply.github.com>
2023-01-08 16:54:36 +0530

    dqw

--- src/config.js
+++ src/config.js
@@ -1,22 +1,22 @@
 const data = [
-    {
-        id: 1,
-        image: "../src/assets/sanitary.png",
-        title: "Learn to use sanitary pads" ,
-        link: "https://github.com/SheBuilds-devs/Unity-Applications"
-    },
-    {
-        id: 2,
-        image: "../src/assets/mensturation.png",
-        title: "Learn about menstrual calendar 🚧👷",
-        link: "" 
-    },
-    {
-        id: 3,
-        image: "../src/assets/selfcare.png",
-        title: "Learn why self care is important 🚧👷",
-        link: "" 
-    },
-]
-export default data
+  {
+    id: 1,
+    image: "/src/assets/sanitary.png",
+    title: "Learn to use sanitary pads",
+    link: "https://github.com/SheBuilds-devs/Unity-Applications",
+  },
+  {
+    id: 2,
+    image: "/assets/mensturation.png",
+    title: "Learn about menstrual calendar 🚧👷",
+    link: "",
+  },
+  {
+    id: 3,
+    image: "/assets/selfcare.png",
+    title: "Learn why self care is important 🚧👷",
+    link: "",
+  },
+];
+export default data;
 // console.log(data)
