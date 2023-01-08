a1ea4703f23e876deaee8de308e168ed4dcb5588
gon araseon <72021422+ravikul505@users.noreply.github.com>
2023-01-08 16:59:04 +0530

    ddew

--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
 const data = [
   {
     id: 1,
-    image: "/src/assets/sanitary.png",
+    image: "../assets/sanitary.png",
     title: "Learn to use sanitary pads",
     link: "https://github.com/SheBuilds-devs/Unity-Applications",
   },
   {
     id: 2,
-    image: "/assets/mensturation.png",
+    image: "../assets/mensturation.png",
     title: "Learn about menstrual calendar 🚧👷",
     link: "",
   },
   {
     id: 3,
-    image: "/assets/selfcare.png",
+    image: "../assets/selfcare.png",
     title: "Learn why self care is important 🚧👷",
     link: "",
   },
